c83d552fb73582f2bf2b29ad80ccd92bf728aad4
todorr92 <todorovicslobodan10@gmail.com>
2020-08-19 11:18:46 +0000

    Added env variables in env.py and left it in gitignore file, same was done with __pycache__ file

--- app.py
+++ app.py
@@ -2,13 +2,15 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from os import path 
+if path.exists("env.py"):
+    import env 
 
 
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = "task_manager"
-app.config["MONGO_URI"] = "mongodb+srv://Todorr:Todorr10@myfirstcluster.gwcei.mongodb.net/task_manager?retryWrites=true&w=majority"
-
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
 
 mongo = PyMongo(app)
 
